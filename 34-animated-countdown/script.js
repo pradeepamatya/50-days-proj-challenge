0f96e2b2d806dca86cafecc1c10f0a3374e5bdfa
pradeepamatya <pradeepbdramatya@gmail.com>
2021-09-08 23:55:16 +0545

    day 35 done html and css

--- 34-animated-countdown/script.js
+++ 34-animated-countdown/script.js
@@ -5,36 +5,39 @@ const replay = document.querySelector('#replay')
 
 runAnimation()
 
-function resetDOM() {
-  counter.classList.remove('hide')
-  finalMessage.classList.remove('show')
+// function resetDOM() {
+//   counter.classList.remove('hide')
+//   finalMessage.classList.remove('show')
 
-  nums.forEach((num) => {
-    num.classList.value = ''
-  })
+//   nums.forEach((num) => {
+//     num.classList.value = ''
+//   })
 
-  nums[0].classList.add('in')
-}
+//   nums[0].classList.add('in')
+// }
 
 function runAnimation() {
   nums.forEach((num, idx) => {
     const nextToLast = nums.length - 1
+    console.log(num, idx);
+    console.log(num);
 
-    num.addEventListener('animationend', (e) => {
-      if (e.animationName === 'goIn' && idx !== nextToLast) {
-        num.classList.remove('in')
-        num.classList.add('out')
-      } else if (e.animationName === 'goOut' && num.nextElementSibling) {
-        num.nextElementSibling.classList.add('in')
-      } else {
-        counter.classList.add('hide')
-        finalMessage.classList.add('show')
-      }
-    })
-  })
-}
+//     num.addEventListener('animationend', (e) => {
+//       if (e.animationName === 'goIn' && idx !== nextToLast) {
+//         num.classList.remove('in')
+//         num.classList.add('out')
+//       } else if (e.animationName === 'goOut' && num.nextElementSibling) {
+//         num.nextElementSibling.classList.add('in')
+//       } else {
+//         counter.classList.add('hide')
+//         finalMessage.classList.add('show')
+//       }
+//     })
+//   })
+// }
 
-replay.addEventListener('click', () => {
-  resetDOM()
-  runAnimation()
+// replay.addEventListener('click', () => {
+//   resetDOM()
+//   runAnimation()
 })
+}
